7fd8764e0f441d72808552397b8cae2c1084754e
Hiddd <Hiddd@LAPTOP-V6MPPD0J>
2025-04-30 16:37:50 +0700

    point 1

---
+++ src/modul1038/Modul1038.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul1038;
+
+/**
+ *
+ * @author Hiddd
+ */
+public class Modul1038 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        int data[][] = {{4,6,4,2,8,4,2,10}, {4,6,4,2,8,4,2,10}};
+        for (int a = 0; a < data[0].length; a++) {
+            for (int b = 0; b < data[0].length; b++) {
+            System.out.print(data[a] [b] + " ");
+        }
+            System.out.println("\n");
+    } 
+    
+    }
+}
